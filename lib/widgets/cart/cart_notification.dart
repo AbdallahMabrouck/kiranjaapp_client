c2949ebd276be7481d9c814607569d4a784693e9
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-11 16:11:12 +0300

    edited cart notification widget

--- lib/widgets/cart/cart_notification.dart
+++ lib/widgets/cart/cart_notification.dart
@@ -18,6 +18,136 @@ class _CartNotificationState extends State<CartNotification> {
     _cartProvider.getCartTotal();
     _cartProvider.getShopName();
 
+    final cartQty = _cartProvider.cartQty;
+    final subTotal = _cartProvider.subTotal;
+    final shopData = _cartProvider.document.data() as Map<String, dynamic>?;
+
+    return Visibility(
+      visible: cartQty > 0,
+      child: Container(
+        height: 45,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(12), topLeft: Radius.circular(12))),
+        child: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "$cartQty ${cartQty == 1 ? " Item" : " Items"}",
+                          style: const TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        const Text(
+                          " | ",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          "\$${subTotal.toStringAsFixed(0)}",
+                          style: const TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    if (shopData != null && shopData.containsKey("shopName"))
+                      Text(
+                        "From  ${shopData["shopName"]}",
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 10),
+                      )
+                  ],
+                ),
+              ),
+              InkWell(
+                onTap: () {
+                  PersistentNavBarNavigator.pushNewScreenWithRouteSettings(
+                    context,
+                    settings: const RouteSettings(name: CartScreen.id),
+                    screen: CartScreen(
+                      document: _cartProvider.document,
+                    ),
+                    withNavBar: false,
+                    pageTransitionAnimation: PageTransitionAnimation.cupertino,
+                  );
+                },
+                child: Container(
+                  child: const Row(
+                    children: [
+                      Text(
+                        "View Cart",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.shopping_bag_outlined,
+                        color: Colors.white,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
+import 'package:provider/provider.dart';
+import '../../providers/cart_provider.dart';
+import '../../screens/cart_screen.dart';
+
+class CartNotification extends StatefulWidget {
+  const CartNotification({super.key});
+
+  @override
+  State<CartNotification> createState() => _CartNotificationState();
+}
+
+class _CartNotificationState extends State<CartNotification> {
+  @override
+  Widget build(BuildContext context) {
+    final _cartProvider = Provider.of<CartProvider>(context);
+    _cartProvider.getCartTotal();
+    _cartProvider.getShopName();
+
     return Visibility(
       visible: _cartProvider.cartQty > 0 ? true : false,
       child: Container(
@@ -102,3 +232,4 @@ class _CartNotificationState extends State<CartNotification> {
     );
   }
 }
+*/
