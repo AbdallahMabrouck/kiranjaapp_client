7b9b56e3db00a536cf1ff922a3278ee347e862af
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-11 20:10:57 +0300

    modified order provider

---
+++ lib/providers/order_provider.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/cupertino.dart';
+
+class OrderProvider with ChangeNotifier {
+  String status;
+
+  // Constructor to initialize the 'status' property
+  OrderProvider({this.status = ""});
+
+  void filterOrder(String status) {
+    this.status = status;
+    notifyListeners();
+  }
+}
+
+
+
+
+
+
+/*import 'package:flutter/cupertino.dart';
+
+class OrderProvider with ChangeNotifier{
+
+  String status;
+
+  filterOrder(status){
+    this.status = status;
+    notifyListeners();
+  }
+
+}*/
