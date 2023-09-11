6b001503d837b9c124ce6877f0cefc099f3a80f4
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-11 12:43:19 +0300

    modified store services

--- lib/services/store_services.dart
+++ lib/services/store_services.dart
@@ -1,14 +1,21 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class StoreServices {
+  CollectionReference vendorbanner =
+      FirebaseFirestore.instance.collection("vendorBanner");
+  CollectionReference vendors =
+      FirebaseFirestore.instance.collection("vendors");
   geTopPickedStore() {
-    return FirebaseFirestore.instance
-        .collection("vendors")
+    return vendors
         .where("accVerified", isEqualTo: true)
         .where("isTopPicked", isEqualTo: true)
         .orderBy("shopName")
         .snapshots();
+    // this will only show verified top picked vendor from admin, sorting stores alphabetically
+  }
+
+  Future<DocumentSnapshot> getShopDetails(sellerUid) async {
+    DocumentSnapshot snapshot = await vendors.doc(sellerUid).get();
+    return snapshot;
   }
 }
-
-// this will only show verified top picked vendor from admin, sorting stores alphabetically
