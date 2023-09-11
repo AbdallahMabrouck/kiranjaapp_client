4933d986dbdcaa9ccc782987c78fc98a353b2c74
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-11 09:46:54 +0300

    created constants widget

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPageViewTextStyle = TextStyle(fontSize: 25, fontWeight: FontWeight.w700);
+
+const kApiKey = "AIzaSyAJRjXWX_uPboe-_HykRpQEZCBEvjpQ6Lg";
+
+const kStoreCardStyle =
+    TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.grey);
--- lib/widgets/banner_widget.dart
+++ lib/widgets/banner_widget.dart
@@ -4,7 +4,7 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:getwidget/components/shimmer/gf_shimmer.dart';
 
-import '../firebase_services.dart';
+import '../services/firebase_services.dart';
 
 class BannerWidget extends StatefulWidget {
   const BannerWidget({super.key});
