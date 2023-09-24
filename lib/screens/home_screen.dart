c581bdb3b1d9e4e60848e283a9cf6f705dbe9423
AbdallahMabrouck <mabrouck32@gmail.com>
2023-09-24 20:11:52 +0300

    modified main.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   children: [
                     // TopPickStore(),
 
-                    CategoryWidget(),
+                    // CategoryWidget(),
                   ],
                 ),
               ),
